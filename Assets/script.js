d8b5def015ae76f3e57b64c37120cd6da66f7c12
Leesa A <leesaali7@gmail.com>
2023-02-25 22:19:28 +0000

    fixed for loop to getItem from local storage

--- Assets/script.js
+++ Assets/script.js
@@ -49,20 +49,21 @@ $(".saveBtn").click(function(event) {
 
 // get descriptions from local storage when page loads.
 
-$("#hour-09").children(".description").val(localStorage.getItem("hour-09"));
-
-for (var i = 10; i < 17; i++) {
+for (var i = 9; i <= 17; i++) {
+  if (i == 9) {
+   $("#hour-09").children(".description").val(localStorage.getItem("hour-09")); 
+  } else {
   $("#hour-" + i).children(".description").val(localStorage.getItem("hour-" + i));
+  }
 }
 
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+// add event listener to clear local storage
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+$("#clearAll").click(function(event) {
+  event.preventDefault();
+  $(".description").val("");
+  localStorage.clear();
+})
+
+
